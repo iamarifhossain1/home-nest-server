c5a44d8a1623b7b6e1e4ef666b0dc23f1f2495af
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-11-14 19:55:26 +0600

    add-properties

--- index.js
+++ index.js
@@ -4,165 +4,183 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
-// MiddleWare
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
 res.send('Home Nest Server Running');
- 
+
 })
 
 const uri = "mongodb+srv://home-nest:nqB8jB2PADweozvT@crud-server.8t1odhz.mongodb.net/?appName=CRUD-Server";
 
 const client = new MongoClient(uri, {
- serverApi: {
- version: ServerApiVersion.v1,
- strict: true,
- deprecationErrors: true,
- }
+serverApi: {
+version: ServerApiVersion.v1,
+strict: true,
+deprecationErrors: true,
+}
 });
 
 async function run () {
- try{
- await client.connect();
- const database = client.db('home_nest')
- const usersCollection = database.collection('users')
- const propertiesCollection = database.collection('properties')
+try{
+await client.connect();
+const database = client.db('home_nest')
+const usersCollection = database.collection('users')
+const propertiesCollection = database.collection('properties')
 
 
- // Users APIs
- app.post('/users', async (req, res) => {
-  const newUser = req.body;
-  const email = req.body.email;
-  const query = {email: email};
-  const existingUser = await usersCollection.findOne(query);
-  if(existingUser) {
-   res.send({message:'user already exist'})
-  }
-  
-  else {
-   const result = await usersCollection.insertOne(newUser);
-  res.send(result);
-  }
-  
- })
+app.post('/users', async (req, res) => {
+ const newUser = req.body;
+ const email = req.body.email;
+ const query = {email: email};
+ const existingUser = await usersCollection.findOne(query);
+ if(existingUser) {
+  res.send({message:'user already exist'})
+ }
+ 
+ else {
+  const result = await usersCollection.insertOne(newUser);
+ res.send(result);
+ }
+ 
+})
 
+   
+   
     
 
-    app.get('/properties', async (req, res) => {
-        
-        const { sort, search } = req.query; 
-        
-        let sortCriteria = {}; 
-        let query = {}; 
+   app.get('/properties', async (req, res) => {
+      
+      const { sort, search } = req.query; 
+      
+      let sortCriteria = {}; 
+      let query = {}; 
 
+      
+      if (search) {
+         
+         const searchRegex = { $regex: search, $options: 'i' }; 
+         
         
-        if (search) {
-            
-            const searchRegex = { $regex: search, $options: 'i' }; 
-            
-           
-            query.$or = [
-                { propertyName: searchRegex },
-                { location: searchRegex },
-                { type: searchRegex }
-            ];
-        }
-        
+         query.$or = [
+            { propertyName: searchRegex },
+            { location: searchRegex },
+            { type: searchRegex }
+         ];
+      }
+      
 
-        switch (sort) {
-            case 'price-asc':
-                sortCriteria = { price: 1 }; 
-                break;
-            case 'price-desc':
-                sortCriteria = { price: -1 }; 
-                break;
-            case 'date-recent':
-                sortCriteria = { createdAt: -1 }; 
-                break;
-            case 'date-old':
-                sortCriteria = { createdAt: 1 }; 
-                break;
-            default:
-                sortCriteria = { createdAt: -1 }; 
-        }
-        
-        try {
-            
-            const cursor = propertiesCollection.find(query).sort(sortCriteria);
-            const result = await cursor.toArray();
-            res.send(result);
-        } catch (error) {
-            console.error("Error fetching sorted/searched properties:", error);
-            res.status(500).send({ message: "Error fetching properties" });
-        }
-    });
+      switch (sort) {
+         case 'price-asc':
+            sortCriteria = { price: 1 }; 
+            break;
+         case 'price-desc':
+            sortCriteria = { price: -1 }; 
+            break;
+         case 'date-recent':
+            sortCriteria = { createdAt: -1 }; 
+            break;
+         case 'date-old':
+            sortCriteria = { createdAt: 1 }; 
+            break;
+         default:
+            sortCriteria = { createdAt: -1 }; 
+      }
+      
+      try {
+         
+         const cursor = propertiesCollection.find(query).sort(sortCriteria);
+         const result = await cursor.toArray();
+         res.send(result);
+      } catch (error) {
+         console.error("Error fetching sorted/searched properties:", error);
+         res.status(500).send({ message: "Error fetching properties" });
+      }
+   });
 
 
- app.get('/featured-properties', async (req, res) => {
-  const cursor = propertiesCollection.find().sort({
+app.get('/featured-properties', async (req, res) => {
+ const cursor = propertiesCollection.find().sort({
 createdAt: -1}).limit(6);
-  const result = await cursor.toArray();
-  res.send(result);
- })
+ const result = await cursor.toArray();
+ res.send(result);
+})
 
 
 app.get('/properties/:id', async (req, res) => {
- const id = req.params.id;
- const query = {_id: new ObjectId(id)};
- 
- const result = await propertiesCollection.findOne(query); 
- 
- if (!result) {
-  return res.status(404).send({ message: "Property not found" });
- }
- 
- res.send(result); 
+const id = req.params.id;
+const query = {_id: new ObjectId(id)};
+
+const result = await propertiesCollection.findOne(query); 
+
+if (!result) {
+ return res.status(404).send({ message: "Property not found" });
+}
+
+res.send(result); 
 })
 
+
+app.post('/properties', async (req, res) =>{
+ const newProduct = req.body;
+ const result = await propertiesCollection.insertOne(newProduct);
+ res.send(result);
+})
+
+app.patch('/properties/:id', async (req, res) => {
+ const id = req.params.id;
+ const updatedData = req.body;
  
- app.post('/properties', async (req, res) =>{
-  const newProduct = req.body;
-  const result = await propertiesCollection.insertOne(newProduct);
-  res.send(result);
- })
+ try {
+   const query = { _id: new ObjectId(id) };
+   
+  
+   const update = {
+    $set: {
+      ...updatedData
+    }
+   };
+   
+   const result = await propertiesCollection.updateOne(query, update);
 
- app.patch('/properties/:id', async (req, res) => {
-  const id = req.params.id;
-  const updatedProperties = req.body;
-  const query = {_id: new ObjectId(id)};
-  const update = {
-   $set: {
-    updatedProperties
+   if (result.matchedCount === 0) {
+    return res.status(404).send({ message: "Property not found." });
    }
-  }
-  const result = await propertiesCollection.updateOne(query, update);
-  res.send(result);
- })
-
- app.delete('/properties/:id', async (req, res) => {
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)};
-  const result = await propertiesCollection.deleteOne(query);
-  res.send(result);
- })
-
-
- await client.db("admin").command({ ping: 1 });
- console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   
+   res.send({
+    success: true,
+    message: "Property updated successfully.",
+    result
+   });
+ 
+ } catch (error) {
+   console.error("Error updating property:", error);
+   res.status(500).send({ message: "Failed to update property." });
  }
+});
 
- finally {
+app.delete('/properties/:id', async (req, res) => {
+ const id = req.params.id;
+ const query = {_id: new ObjectId(id)};
+ const result = await propertiesCollection.deleteOne(query);
+ res.send(result);
+})
 
- }
+
+await client.db("admin").command({ ping: 1 });
+console.log("Pinged your deployment. You successfully connected to MongoDB!");
+}
+
+finally {
+
+}
 }
 
 run().catch(console.dir)
 
 
-// Listen Port
 app.listen(port, () => {
- console.log(`Server running on Port: ${port}`);
- 
+console.log(`Server running on Port: ${port}`);
+
 })
